32e8bfc32e792c89b30ca9a18df4d66c540aa884
aliselmes <a.selmes@gmail.com>
2020-12-09 20:21:51 -0600

    Fetch From Server

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,7 +1,16 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionsTypes from './ActionTypes';
 
-export const Promotions = (state = PROMOTIONS, action) => {
+export const Promotions = (state = { isLoading: true, errMess: null, promotions: []}, action) => {
     switch (action.type) {
+        case ActionsTypes.ADD_PROMOTIONS:
+            return {...state, isLoading: false, errMess: null, promotions: action.payload};
+
+        case ActionsTypes.PROMOTIONS_LOADING:
+            return {...state, isLoading: true, erMess: null, promotions: []};
+
+        case ActionsTypes.PROMOTIONS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+            
         default:
             return state;
     }
